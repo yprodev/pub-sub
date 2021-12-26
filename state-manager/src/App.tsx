d913db98e595ee9cf391c4110774d3a83a6abead
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2021-12-26 06:03:44 +0200

    Create react app with simple state managers with the list of subscribers as a reference in a custom hook

---
+++ state-manager/src/App.tsx
@@ -0,0 +1,29 @@
+import { createStateHook } from './simpleStateManager'
+
+const useCounter = createStateHook(0)
+
+const Counter = () => {
+  const [count, setCount] = useCounter()
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>Add One</button>
+      <div>Count = {count}</div>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className='App'>
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+    </div>
+  )
+}
+
+export default App
